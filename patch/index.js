0dd0ee4a6375a74da6de34fbd71cb21ca2a32ca4
soulteary <soulteary@gmail.com>
2016-08-29 13:42:18 +0800

    support check path file checksum, imporve log format

--- patch/index.js
+++ patch/index.js
@@ -2,6 +2,10 @@
 
 'use strict';
 
+const moduleName = '[hexo-document-plugin@patch-index]';
+const log = console.log.bind(console, moduleName);
+const error = console.error.bind(console, moduleName);
+
 const fs = require('story-fs');
 const path = require('path');
 const backup = require('../lib/backup');
@@ -13,19 +17,23 @@ let hexoUtilBaseDir = path.resolve(__dirname, '../../hexo-util');
 const hijackPathList = [
   {
     src: path.resolve(__dirname, 'hexo/lib/plugins/helper/index.js'),
-    dist: path.resolve(hexoBaseDir, 'lib/plugins/helper/index.js')
+    dist: path.resolve(hexoBaseDir, 'lib/plugins/helper/index.js'),
+    md5: '5829B69AF2262825D4731ECC86B46A49'
   },
   {
     src: path.resolve(__dirname, 'hexo/lib/plugins/generator/page.js'),
-    dist: path.resolve(hexoBaseDir, 'lib/plugins/generator/page.js')
+    dist: path.resolve(hexoBaseDir, 'lib/plugins/generator/page.js'),
+    md5: 'AE10C4F1BFE39E37983CFB9987968529'
   },
   {
     src: path.resolve(__dirname, 'hexo/lib/plugins/console/index.js'),
-    dist: path.resolve(hexoBaseDir, 'lib/plugins/console/index.js')
+    dist: path.resolve(hexoBaseDir, 'lib/plugins/console/index.js'),
+    md5: '699A45B14E2197ABC2C162A779CD9275'
   },
   {
     src: path.resolve(__dirname, 'hexo-util/lib/permalink.js'),
-    dist: path.resolve(hexoUtilBaseDir, 'lib/permalink.js')
+    dist: path.resolve(hexoUtilBaseDir, 'lib/permalink.js'),
+    md5: '9E2BD212DC61875F30EE6D8D3A95ED98'
   }
 ];
 
@@ -38,13 +46,15 @@ switch (process.argv[2].slice(2)) {
     action = restore;
     break;
   default:
-    console.log('Hexo-document-plugin only support `backup` or `restore` files.');
+    log('current only support `backup` or `restore` files.');
     process.exit(1);
     break;
 }
 
 Promise.all(hijackPathList.map(action)).then(function() {
-  console.info('execute patch done.');
+  log('execute patch done.');
 }).catch(function(e) {
-  throw 'Execute patch fail:' + e;
+  let err = 'Execute patch fail:' + e;
+  error(err);
+  throw err;
 });
